068909e9e2ba0731d31e3adff4d305f21d8f9f4c
Eduard Tatarnikov <edmaroff@gmail.com>
2023-02-01 14:07:24 +0800

    Extra HW dj-1.2 For review

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,27 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+from pagination import settings
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
+with open(settings.BUS_STATION_CSV, encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    CONTENT = list(reader)
+
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(CONTENT, 10)
+    page = paginator.get_page(page_number)
+    bus_stations = list(page)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': bus_stations,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
