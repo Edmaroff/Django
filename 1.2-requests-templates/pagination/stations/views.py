7a0790fbf18b696a7a9fe8015ef20534a19d89b5
Eduard Tatarnikov <edmaroff@gmail.com>
2023-02-04 15:47:33 +0800

    Extra HW dj-1.2 corrected_views

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -10,12 +10,12 @@ from pagination import settings
 def index(request):
     return redirect(reverse('bus_stations'))
 
-with open(settings.BUS_STATION_CSV, encoding='utf-8') as f:
-    reader = csv.DictReader(f)
-    CONTENT = list(reader)
 
 
 def bus_stations(request):
+    with open(settings.BUS_STATION_CSV, encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        CONTENT = list(reader)
     page_number = int(request.GET.get('page', 1))
     paginator = Paginator(CONTENT, 10)
     page = paginator.get_page(page_number)
